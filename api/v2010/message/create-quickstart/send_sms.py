f5e8e8c0812fc3bb2b10b98b46cf9a278a08d881
pikios <48543177+pikios@users.noreply.github.com>
2019-03-14 16:17:33 +1100

    Update and rename create-quickstart.6.x.py to send_sms.py

--- api/v2010/message/create-quickstart/send_sms.py
+++ api/v2010/message/create-quickstart/send_sms.py
@@ -4,15 +4,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-auth_token = 'your_auth_token'
-client = Client(account_sid, auth_token)
+account_sid = 'ACedb3b56153753f02d574b13a580e9cc7'
+auth_token = 'bf595497196f4a948fcba2af185e7e1c'
+client = Client(ACedb3b56153753f02d574b13a580e9cc7, bf595497196f4a948fcba2af185e7e1c)
 
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+15017122661',
-                     to='+15558675310'
+                     from_='+61488843000',
+                     to='+61409351632'
                  )
 
 print(message.sid)
